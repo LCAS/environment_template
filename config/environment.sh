faa98a4cab7bafe90f5a3d971bcb63e46af8584c
James R Heselden <primordia@live.com>
2023-09-09 00:08:57 +0100

    enviornment.sh update

--- config/environment.sh
+++ config/environment.sh
@@ -1,6 +1,6 @@
-export FIELD_NAME='main'
-export FIELD_ID='0'
-export FIELD_TYPE='base'
+export FIELD_NAME='riseholme_bullock_close'
+export FIELD_ID='2930'
+export FIELD_TYPE='pasture'
 
 export MQTT_IP='0.0.0.0'
 export MQTT_PORT='8883'
