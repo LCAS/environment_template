d6b7b69df5b106dd72ef36efe4e1e4e674badc13
James R Heselden <primordia@live.com>
2023-10-01 22:02:11 +0100

    better environment.sh

--- config/environment.sh
+++ config/environment.sh
@@ -1,9 +1,20 @@
+# Farm Details
 export FARM_NAME='riseholme_park'
 
-export FIELD_NAME='riseholme_strawberry_polytunnel'
-export FIELD_ID='p1'
-export FIELD_TYPE='polytunnels'
+# Field Details
+export FIELD_NAME='strawberry_polytunnel'
+export FIELD_ID='poly1'
+export FIELD_TYPE='polytunnel'
 
-export MQTT_IP='0.0.0.0'
-export MQTT_PORT='8883'
-export MQTT_NS=''
+# Config Quick-References
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+
+export DATUM_FILE="$CONF/location/datum.yaml"
+if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
+
+export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
+
+
+
+
