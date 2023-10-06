c4bd41ec35f0dd189a48869f9a130b17917f2621
James R Heselden <primordia@live.com>
2023-10-06 12:26:40 +0100

    typo

--- config/environment.sh
+++ config/environment.sh
@@ -7,7 +7,7 @@ export FIELD_ID='poly1'
 export FIELD_TYPE='polytunnel'
 
 # Config Quick-References
-CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config
 
 export DATUM_FILE="$CONF/location/datum.yaml"
 if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
