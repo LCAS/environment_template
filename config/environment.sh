172251ea49320a1219e15af27c6604cdd46f631d
James R Heselden <primordia@live.com>
2023-10-06 12:27:35 +0100

    inclusion of aytogen files

--- config/environment.sh
+++ config/environment.sh
@@ -1,18 +1,16 @@
-# Field Details
-export FIELD_NAME='main'
-export FIELD_ID='0'
-export FIELD_TYPE='base'
+# Farm Details
+export FARM_NAME='riseholme_park'
 
-# Connection details
-export MQTT_IP='0.0.0.0'
-export MQTT_PORT='8883'
-export MQTT_NS=''
+# Field Details
+export FIELD_NAME='general_central_pathway'
+export FIELD_ID='p2'
+export FIELD_TYPE='pathway'
 
 # Config Quick-References
-#CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
-#
-#export DATUM_FILE="$CONF/location/datum.yaml"
-#if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
-#
-#export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
-#if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+
+export DATUM_FILE="$CONF/location/datum.yaml"
+if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
+
+export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
