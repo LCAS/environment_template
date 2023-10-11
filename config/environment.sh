63550446223f92a28a5d3c56abd7e363e75ca435
James R Heselden <primordia@live.com>
2023-10-11 11:17:12 +0100

    map update

--- config/environment.sh
+++ config/environment.sh
@@ -2,12 +2,12 @@
 export FARM_NAME='riseholme_park'
 
 # Field Details
-export FIELD_NAME='general_central_pathway'
-export FIELD_ID='p2'
+export FIELD_NAME='general_south_pathway'
+export FIELD_ID='p3'
 export FIELD_TYPE='pathway'
 
 # Config Quick-References
-CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config
 
 export DATUM_FILE="$CONF/location/datum.yaml"
 if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
