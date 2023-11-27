a1d0d33d942d9a64970777e3095f4fb7cd4b549e
James R Heselden <primordia@live.com>
2023-11-27 17:37:28 +0000

    maps included

--- config/environment.sh
+++ config/environment.sh
@@ -1,16 +1,13 @@
 # Farm Details
-export FARM_NAME='uol'
+export FARM_NAME='riseholme_park'
 
 # Field Details
-export FIELD_NAME='main'
-export FIELD_ID='uuid'
-export FIELD_TYPE='empty'
+export FIELD_NAME='risheolme_hall_east'
+export FIELD_ID='0'
+export FIELD_TYPE='building'
 
 # Config Quick-References
-#CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
 
-#export DATUM_FILE="$CONF/location/datum.yaml"
-#if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
-
-#export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
-#if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
+export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
