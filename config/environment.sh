11240d3c3e26574cbbadebf158ed226147746631
JamesH <primordia@live.com>
2023-12-18 16:47:11 +0000

    env config update

--- config/environment.sh
+++ config/environment.sh
@@ -2,12 +2,12 @@
 export FARM_NAME='riseholme_park'
 
 # Field Details
-export FIELD_NAME='risheolme_hall_east'
+export FIELD_NAME='riseholme_hall_east'
 export FIELD_ID='0'
 export FIELD_TYPE='building'
 
 # Config Quick-References
-CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config
 
 export MAP_FILE="$CONF/metric/map/map.yaml"
 if [ ! -f "$MAP_FILE" ]; then export MAP_FILE="$CONF/metric/map_autogen/map.yaml" ; fi
