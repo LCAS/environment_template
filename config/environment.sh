4959dbf3cf887b41db55ea55daf915cd590357a0
JamesH <primordia@live.com>
2024-02-12 09:42:24 +0000

    group name added

--- config/environment.sh
+++ config/environment.sh
@@ -1,12 +1,10 @@
+# Farm Details
+export FARM_NAME='none'
+
 # Field Details
 export FIELD_NAME='main'
 export FIELD_ID='0'
 export FIELD_TYPE='base'
-
-# Connection details
-export MQTT_IP='0.0.0.0'
-export MQTT_PORT='8883'
-export MQTT_NS=''
 
 # Config Quick-References
 #CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
